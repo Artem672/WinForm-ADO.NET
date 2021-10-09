a5eccb8b0b52af4f15aa738742e342d0e19250f6
Artem672 <synytsiaartem07@gmail.com>
2021-10-09 18:30:10 +0300

    Explorer task

---
+++ Explorer/Explorer/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace Explorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.leftTV = new System.Windows.Forms.TreeView();
+            this.filesLV = new System.Windows.Forms.ListView();
+            this.fileDIrectoryLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // leftTV
+            // 
+            this.leftTV.Location = new System.Drawing.Point(13, 12);
+            this.leftTV.Name = "leftTV";
+            this.leftTV.Size = new System.Drawing.Size(221, 426);
+            this.leftTV.TabIndex = 0;
+            this.leftTV.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.leftTV_AfterSelect);
+            this.leftTV.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.leftTV_NodeMouseDoubleClick);
+            // 
+            // filesLV
+            // 
+            this.filesLV.HideSelection = false;
+            this.filesLV.Location = new System.Drawing.Point(241, 37);
+            this.filesLV.Name = "filesLV";
+            this.filesLV.Size = new System.Drawing.Size(536, 401);
+            this.filesLV.TabIndex = 1;
+            this.filesLV.UseCompatibleStateImageBehavior = false;
+            this.filesLV.SelectedIndexChanged += new System.EventHandler(this.filesLV_SelectedIndexChanged);
+            this.filesLV.KeyDown += new System.Windows.Forms.KeyEventHandler(this.filesLV_KeyDown);
+            this.filesLV.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.filesLV_MouseDoubleClick);
+            // 
+            // fileDIrectoryLabel
+            // 
+            this.fileDIrectoryLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.fileDIrectoryLabel.Location = new System.Drawing.Point(241, 12);
+            this.fileDIrectoryLabel.Name = "fileDIrectoryLabel";
+            this.fileDIrectoryLabel.Size = new System.Drawing.Size(536, 22);
+            this.fileDIrectoryLabel.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.fileDIrectoryLabel);
+            this.Controls.Add(this.filesLV);
+            this.Controls.Add(this.leftTV);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView leftTV;
+        private System.Windows.Forms.ListView filesLV;
+        private System.Windows.Forms.Label fileDIrectoryLabel;
+    }
+}
+
---
+++ Explorer/Explorer/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Explorer
+{
+    public partial class Form1 : Form
+    {
+        //private static readonly string defaultPath = Environment.GetFolderPath(System.Environment.SpecialFolder.MyComputer);
+        private List<string> Logs { get; set; } = new List<string>();
+        public string previousFolderPath { get; set; }
+        public string newFolderPath { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+            string[] root = Directory.GetLogicalDrives();//@"D:\";
+            ImageList imageList = new ImageList();
+            imageList.Images.Add(Image.FromFile(@"icon.jpg"));
+            leftTV.ImageList = imageList;
+            leftTV.ImageIndex = 0;
+
+            foreach (var item in root)
+            {   
+                leftTV.Nodes.Add(item);
+            }
+        }
+
+        private void leftTV_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            try
+            {
+                var selectedPath = leftTV.SelectedNode.FullPath;
+                //fileDIrectoryLabel.Text = selectedPath;
+                var folders = Directory.GetDirectories(selectedPath)
+                                    .Select(Path.GetFileName)
+                                    .ToList();
+
+                foreach (var item in folders)
+                {
+                    leftTV.SelectedNode.Nodes.Add(new TreeNode(item));
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+        private void RefreshList(string selectedPath)
+        {
+            filesLV.Items.Clear();
+            DirectoryInfo directoryInfo = new DirectoryInfo(leftTV.SelectedNode.FullPath);
+            var files = Directory.GetFiles(selectedPath)
+                                .Select(Path.GetFileName)
+                                .ToList();
+            var folders = Directory.GetDirectories(selectedPath)
+                                .Select(Path.GetFileName)
+                                .ToList();
+            var imageList = new ImageList();
+
+            imageList.Images.Add(Image.FromFile(@"file.png"));
+            imageList.Images.Add(Image.FromFile(@"icon.jpg"));
+            imageList.ImageSize = new Size(32, 32);
+            filesLV.View = View.LargeIcon;
+            filesLV.LargeImageList = imageList;
+
+            foreach (var item in folders)
+            {
+                var lvItem = new ListViewItem(item);
+                lvItem.ImageIndex = 1;
+                filesLV.Items.Add(lvItem);
+            }
+
+            foreach (var item in files)
+            {
+                var lvItem = new ListViewItem(item);
+                lvItem.ImageIndex = 0;
+                filesLV.Items.Add(lvItem);
+            }
+        }
+        private void leftTV_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            try
+            {               
+                var selectedPath = leftTV.SelectedNode.FullPath;
+                Logs.Add(selectedPath);
+                previousFolderPath = selectedPath;
+                RefreshList(selectedPath);
+                fileDIrectoryLabel.Text = selectedPath;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void filesLV_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if(e.KeyCode == Keys.Delete && filesLV.SelectedItems.Count > 0)
+                {
+                    for (var i = 0; i < filesLV.SelectedItems.Count; i++)
+                    {
+                        var selectedPath = leftTV.SelectedNode.FullPath + "\\" + filesLV.SelectedItems[i].Text;
+                        if(File.Exists(selectedPath))
+                        {
+                            FileInfo fileInfo = new FileInfo(selectedPath);
+                            File.SetAttributes(fileInfo.FullName, FileAttributes.Normal);
+                            File.Delete(selectedPath);
+                        }
+                        else
+                        {
+                            Directory.Delete(selectedPath);
+                        }
+                    }
+                    RefreshList(leftTV.SelectedNode.FullPath);
+                }
+                else if(e.KeyCode == Keys.Back && Logs.Count > 1)
+                {
+                    Logs.RemoveAt(Logs.Count - 1);
+                    fileDIrectoryLabel.Text = Logs[Logs.Count - 1];
+                    RefreshList(Logs[Logs.Count - 1]);
+                }
+                               
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void filesLV_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if(filesLV.SelectedItems.Count == 1)
+                {
+                    var selectedPath = leftTV.SelectedNode.FullPath;
+                    fileDIrectoryLabel.Text = previousFolderPath + "\\" +filesLV.SelectedItems[0].Text;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void filesLV_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                if(filesLV.SelectedItems.Count > 0)
+                {                   
+                    var selectedPath = leftTV.SelectedNode.FullPath + "\\" + filesLV.SelectedItems[0].Text;
+                    if(File.Exists(selectedPath))
+                    {
+                        var thread = new Thread(() => { Process.Start(selectedPath); });
+                        thread.Start();
+                    }
+                    else
+                    {
+                        previousFolderPath += ("\\" + filesLV.SelectedItems[0].Text);
+                        Logs.Add(previousFolderPath);
+                        RefreshList(previousFolderPath);                       
+                    }
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
